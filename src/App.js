a60d36ca8ba4c29ae3ad0187c8f06ee74e03bb06
matthewvolny <matthew.volny@gmail.com>
2022-11-12 10:06:26 -0500

    values show in display

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import "./App.css";
+import { useState } from "react";
 import { Button } from "./components/Button";
 
 function App() {
-  const buttonValues = [
-    "AC",
-    "( )",
-    "\u02C4",
+  const [calc, setCalc] = useState("");
+  const [result, setResult] = useState("");
+
+  const updateCalculation = (value) => {
+    let operators = ["\u00F7", "x", "-", "+", "."];
+    if (
+      operators.includes(value) &&
+      operators.includes(calc[calc.length - 1])
+    ) {
+      return;
+    }
+    setCalc(calc + value);
+  };
+
+  const calculateResult = () => {
+    setResult(calc);
+    console.log(calc);
+  };
+
+  const basicValues = [
     "\u00F7",
     7,
     8,
@@ -21,20 +38,54 @@ function App() {
     "+",
     0,
     ".",
-    "Del",
-    "=",
   ];
 
   return (
     <div className="App">
       <div className="calculator">
         <div className="display">
-          <span>0</span>
+          <span>{calc || "0"}</span>
         </div>
         <div className="buttons-container">
-          {buttonValues.map((button, i) => {
-            return <Button key={i} button={button} />;
+          <button
+            className="purple-button"
+            onClick={() => updateCalculation("AC")}
+          >
+            AC
+          </button>
+          <button
+            className="purple-button"
+            onClick={() => updateCalculation("( )")}
+          >
+            ( )
+          </button>
+          <button
+            className="purple-button"
+            onClick={() => updateCalculation("\u02C4")}
+          >
+            {"\u02C4"}
+          </button>
+          {basicValues.map((button, i) => {
+            return (
+              <Button
+                key={i}
+                button={button}
+                updateCalculation={updateCalculation}
+              />
+            );
           })}
+          <button
+            className="purple-button"
+            onClick={() => updateCalculation("Del")}
+          >
+            Del
+          </button>
+          <button
+            className="purple-button"
+            onClick={() => calculateResult("=")}
+          >
+            =
+          </button>
         </div>
       </div>
     </div>
--- src/components/Button.js
+++ src/components/Button.js
@@ -2,18 +2,43 @@ import React from "react";
 
 export const Button = (props) => {
   if (
-    props.button === "AC" ||
-    props.button === "( )" ||
-    props.button === "\u02C4" ||
+    // props.button === "AC" ||
+    // props.button === "( )" ||
+    // props.button === "\u02C4" ||
     props.button === "\u00F7" ||
     props.button === "x" ||
     props.button === "-" ||
     props.button === "+"
   ) {
-    return <button className="purple-button">{props.button}</button>;
-  } else if (props.button === "=" || props.button === "Del") {
-    return <button className="shaded-button">{props.button}</button>;
-  } else {
-    return <button>{props.button}</button>;
+    return (
+      <button
+        className="purple-button"
+        value={props.button}
+        onClick={() => props.updateCalculation(props.button)}
+      >
+        {props.button}
+      </button>
+    );
+  }
+  //   else if (props.button === "=" || props.button === "Del") {
+  //     return (
+  //       <button
+  //         className="shaded-button"
+  //         value={props.button}
+  //         onClick={() => props.updateCalculation(props.button)}
+  //       >
+  //         {props.button}
+  //       </button>
+  //     );
+  //   }
+  else {
+    return (
+      <button
+        value={props.button}
+        onClick={() => props.updateCalculation(props.button)}
+      >
+        {props.button}
+      </button>
+    );
   }
 };
